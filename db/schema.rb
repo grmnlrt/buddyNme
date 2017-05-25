cffcda3a68bc40302f9108d97b870fedded13ce2
florestan lefevre <florestanlefevre@gmail.com>
2017-05-25 19:37:21 +0200

    cleaned

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 20170525172837) do
     t.float    "longitude"
     t.float    "latitude"
     t.boolean  "book"
-    t.json     "photos"
     t.index ["user_id"], name: "index_missions_on_user_id", using: :btree
   end
 
