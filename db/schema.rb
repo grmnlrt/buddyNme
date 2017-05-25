ef46746e61ddf765f8840ad0277405ea42b53abc
florestan lefevre <florestanlefevre@gmail.com>
2017-05-25 19:29:41 +0200

    cleaned photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525170057) do
+ActiveRecord::Schema.define(version: 20170525172837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20170525170057) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.json     "photos"
+    t.string   "photo"
     t.datetime "date"
     t.string   "address"
     t.float    "longitude"
