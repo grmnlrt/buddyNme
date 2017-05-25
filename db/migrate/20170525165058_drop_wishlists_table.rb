3ced31e6ef3f116418b9d5b4bc4f19e4b3c4e352
florestan lefevre <florestanlefevre@gmail.com>
2017-05-25 19:11:18 +0200

    cleaned

--- db/migrate/20170525165058_drop_wishlists_table.rb
+++
@@ -1,8 +0,0 @@
-class DropWishlistsTable < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :wishlists do |t|
-      t.string :name, null: false
-      t.timestamps null: false
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525151723) do
+ActiveRecord::Schema.define(version: 20170525170057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,25 +75,8 @@ ActiveRecord::Schema.define(version: 20170525151723) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
-  create_table "wants", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "mission_id"
-    t.index ["mission_id"], name: "index_wants_on_mission_id", using: :btree
-  end
-
-  create_table "wishlists", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "name"
-    t.integer  "user_id"
-    t.index ["user_id"], name: "index_wishlists_on_user_id", using: :btree
-  end
-
   add_foreign_key "bookings", "missions"
   add_foreign_key "bookings", "users"
   add_foreign_key "missions", "users"
   add_foreign_key "reviews", "users"
-  add_foreign_key "wants", "missions"
-  add_foreign_key "wishlists", "users"
 end
